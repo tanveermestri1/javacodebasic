9da7f9d5c06f399f4eaeaf0212a904f1d9ca1ec0
Tanveer Mestri <tanveer.mestri555@gmail.com>
2025-10-09 20:12:20 +0530

    java code added

---
+++ Arithmatic_Operator.java
@@ -0,0 +1,13 @@
+package basicprogram;
+
+public class Arithmatic_Operator
+{
+	public static void main(String[] args)
+	{
+		int a=50;
+		int b=60;
+		int c=a+b;
+		System.out.println("Addition is "+c);
+	}
+
+}
---
+++ Arithmatic_Operator1.java
@@ -0,0 +1,14 @@
+package basicprogram;
+
+public class Arithmatic_Operator1 {
+
+	public static void main(String[] args) 
+	{
+		int a=50;
+		int b=60;
+		int c=b-a;
+		System.out.println("Addition is "+c);
+
+	}
+
+}
